a573fede3a0a74d72f3ade011771c429de18f562
Rogério A. Oliveira <rogerioalexandredeoliveira87@gmail.com>
2022-04-05 15:06:43 -0300

    Adding database access

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -14,7 +14,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import model.entities.Department;
+import model.entites.Department;
 import model.services.DepartmentSevice;
 
 public class DepartmentListController implements Initializable{
